b90a0e0856806b9f01db72ce0022eb1e6ebc5b3b
Tristan Galloway <tristan.a.galloway@gmail.com>
2025-06-13 13:04:54 -0600

    Working ensureAuthenticated middleware.

---
+++ middleware/auth.js
@@ -0,0 +1,8 @@
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated && req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ error: 'Unauthorized. Please log in.' });
+}
+
+module.exports = ensureAuthenticated;
